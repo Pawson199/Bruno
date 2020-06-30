a5f63224a772bcdd77927c0a55f702ae7acc379a
Pawson199 <starnaw1998@interia.pl>
2020-06-30 18:46:19 +0200

    Add new styles to homepage - offers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ import React from 'react'
 import fb from '../images2/fb.png'
 import pint from '../images2/pint.png'
 import ig from '../images2/ig.png'
-import pies1 from '../images2/pies1.jpg'
-import pies2 from '../images2/pies2.jpg'
-import pies3 from '../images2/pies3.jpg'
+import pies1 from '../images2/pj1.jpg'
+import pies2 from '../images2/pj2.jpg'
+import pies3 from '../images2/pj3.jpg'
 import pies4 from '../images2/pjesek_2.png'
 import {Button} from '../components/Button'
 import Homeoffer from '../components/Homeoffer'
@@ -12,6 +12,8 @@ import Homeoffer from '../components/Homeoffer'
 export default function Home() {
     return (
         <div className="view">
+            <div className="clippy1" ></div>
+            <div className="clippy2" ></div>
             <div className="social-media-and-description" >
                 <div className="description">
                     <h1>Witaj w świecie <br/>
@@ -30,12 +32,12 @@ export default function Home() {
                 </div>
             </div>
             <div className="home_offers">
-                <Homeoffer src={pies1} desc="Pies1" />
-                <Homeoffer src={pies2} desc="pIES2" />
-                <Homeoffer src={pies3} desc="PIES3" /> 
+                <Homeoffer src={pies2} desc="Oferujemy grawerna zamówienie!" />
+                <Homeoffer src={pies1} desc="Obroża na każdy rozmiar!" />
+                <Homeoffer src={pies3} desc="Sprzedajemy również zestawy ze smyczą!" /> 
             </div>
             <div className="mobile_bg_container" >
-                <img src={pies4} alt="dog_image" ></img>
+                <img src={pies4} alt="dog" ></img>
             </div>
         </div>
     )
