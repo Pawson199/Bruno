24471c5d4c538a3761b3302ef6c9c62c25161694
Pawson199 <starnaw1998@interia.pl>
2020-07-07 14:05:40 +0200

    fixed bug with .env

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -5,6 +5,33 @@ import {change_category} from '../redux'
 
 function Offers(props) {
 
+
+fetch(
+  'https://graphql.datocms.com/',
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${process.env.REACT_APP_API_PRODUCTS_KEY}`,
+    },
+    body: JSON.stringify({
+      query: '{ allBluzas { cena nazwa opis zdjCie { url } } }'
+    }),
+  }
+)
+.then(res => res.json())
+.then((res) => {
+
+    res.data.allBluzas.forEach( el => {
+        console.log(el)
+    } )
+})
+.catch((error) => {
+  console.log(error);
+});
+
+
     return (
         <div className="offers_container">
             <Select 
