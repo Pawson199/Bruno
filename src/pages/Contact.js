330f8fcfd09ca5ba535320932e4dc50be194d336
Pawson199 <starnaw1998@interia.pl>
2020-07-09 21:29:36 +0200

    Another bug with form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     return (
         <div className="contact_container">
             <form name="contact" method="POST" data-netlify="true">
-                    <input type="hidden" name="contact" value="contact" />
+                    <input type="hidden" name="form-name" value="contact" />
                     <p className="paragraph">
                         <label>E-mail <input type="email" name="name" /></label>
                     </p>
