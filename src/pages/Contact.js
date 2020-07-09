cb4739ac2587d5ee4d47b9a580c2667edc350bf6
Pawson199 <starnaw1998@interia.pl>
2020-07-09 16:21:46 +0200

    Add hidden form for netlify forms

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ import {Button} from '../components/Button'
 export default function Contact() {
     return (
         <div className="contact_container">
-            <form name="contact" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true">
                     <p className="paragraph">
                         <label>E-mail <input type="email" name="name" /></label>
                     </p>
@@ -12,13 +12,13 @@ export default function Contact() {
                         <label>Treść Wiadomości <textarea type="message" name="email" /></label>
                     </p>
                     <div className="policy">
-                        <p className="paragraph">Wyrażam zgodę na przetwarzanie moich danych osobowych przez home.pl S.A., ul. Zbożowa 4,
-                            70-653 Szczecin w celu i w zakresie niezbędnym do realizacji obsługi niniejszego zgłoszenia.
+                        <p className="paragraph">Wyrażam zgodę na przetwarzanie moich danych osobowych przez Bruno Leatherworks Wojciech Bednarek Rokicińska 476 
+                        92-601 Łódź w celu i w zakresie niezbędnym do realizacji obsługi niniejszego zgłoszenia.
                             Zapoznałem się z treścią informacji o sposobie przetwarzania moich danych osobowych jak w załączniku.
                         </p>
                         <input type="checkbox" id="scales" name="scales"/>
                     </div>
-                        <Button><button onClick={ (e) => e.preventDefault() } type="submit"><p >Wyślij</p></button></Button>
+                    <Button><button onClick={ (e) => e.preventDefault() } type="submit"><p >Wyślij</p></button></Button>
             </form>
         </div>
     )
