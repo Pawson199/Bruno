88c9c89002fe66aaa564e6ad6f7d2f33de616410
Pawson199 <starnaw1998@interia.pl>
2020-07-10 13:12:04 +0200

    Add compltly new form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,27 +1,11 @@
 import React from 'react'
-import {Button} from '../components/Button'
+import Form from '../components/Form'
 
 export default function Contact() {
     
     return (
         <div className="contact_container">
-            <form name="contact" method="post" data-netlify="true">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <p className="paragraph">
-                        <label>E-mail <input type="email" name="email" /></label>
-                    </p>
-                    <p className="paragraph">
-                        <label>Treść Wiadomości <textarea name="message" /></label>
-                    </p>
-                    <div className="policy">
-                        <p className="paragraph">Wyrażam zgodę na przetwarzanie moich danych osobowych przez Bruno Leatherworks Wojciech Bednarek Rokicińska 476 
-                        92-601 Łódź w celu i w zakresie niezbędnym do realizacji obsługi niniejszego zgłoszenia.
-                            Zapoznałem się z treścią informacji o sposobie przetwarzania moich danych osobowych jak w załączniku.
-                        </p>
-                        <input type="checkbox" id="scales" name="scales"/>
-                    </div>
-                    <Button><button type="submit" ><p >Wyślij</p></button></Button>
-            </form>
+            <Form/>
         </div>
     )
 }
